5b487e9a6d798e2a8f605531d6fee7b39d127182
liyibing28 <liyibing28@gmail.com>
2018-05-14 14:58:14 +0800

    整理vuex

--- resources/assets/js/store/modules/login.js
+++ resources/assets/js/store/modules/login.js
@@ -1,6 +1,6 @@
 import jwtToken from './../../helpers/jwt'
 import jwt from "../../helpers/jwt";
-import * as types from "../mutation-type";
+import * as types from "../mutation-types";
 
 export default {
     actions :  {
